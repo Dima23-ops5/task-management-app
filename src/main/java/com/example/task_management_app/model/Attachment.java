6e4fef8d2c1afa1d88edc8cb0e3d09979e2466aa
Dmytro Makhanok <dmmhnk23@gmail.com>
2024-10-09 23:30:57 +0200

    added models and theis reletionships

---
+++ src/main/java/com/example/task_management_app/model/Attachment.java
@@ -0,0 +1,38 @@
+package com.example.task_management_app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "attachments")
+@SQLDelete(sql = "UPDATE attachments SET is_deleted = TRUE WHERE id = ?")
+@SQLRestriction(value = "is_deleted = FALSE")
+public class Attachment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "task_id")
+    private Task task;
+    @Column(nullable = false)
+    private String dropboxFileId;
+    @Column(nullable = false)
+    private String fileName;
+    @Column(nullable = false)
+    private LocalDateTime uploadDate;
+    @Column(nullable = false)
+    private Boolean isDeleted = false;
+}
---
+++ src/main/java/com/example/task_management_app/model/Project.java
@@ -0,0 +1,53 @@
+package com.example.task_management_app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "projects")
+@SQLDelete(sql = "UPDATE projects SET is_deleted = TRUE WHERE id = ?")
+@SQLRestriction(value = "is_deleted = FALSE")
+@NoArgsConstructor
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private LocalDate startDate;
+    @Column(nullable = false)
+    private LocalDate endDate;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    @OneToMany(mappedBy = "project")
+    private Set<Task> tasks = new HashSet<>();
+    @Column(nullable = false)
+    private Boolean isDeleted = false;
+
+    public enum Status {
+        INITIATED,
+        IN_PROGRESS,
+        COMPLETED
+    }
+}
