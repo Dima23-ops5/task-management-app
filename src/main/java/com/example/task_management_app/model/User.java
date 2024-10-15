42eb6341ca7b1026b26519e8df11bca3a0a93751
Dmytro Makhanok <dmmhnk23@gmail.com>
2024-10-15 22:36:55 +0200

    add mysql connection and liqubase

--- src/main/java/com/example/task_management_app/model/User.java
+++ src/main/java/com/example/task_management_app/model/User.java
@@ -2,9 +2,13 @@ package com.example.task_management_app.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.HashSet;
@@ -23,6 +27,7 @@ import org.hibernate.annotations.SQLRestriction;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
     private Long id;
     @Column(nullable = false)
     private String userName;
@@ -38,6 +43,13 @@ public class User {
     private Set<Task> tasks = new HashSet<>();
     @OneToMany(mappedBy = "user")
     private Set<Comment> comments = new HashSet<>();
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> role = new HashSet<>();
     @Column(nullable = false)
     private Boolean isDeleted = false;
 }
