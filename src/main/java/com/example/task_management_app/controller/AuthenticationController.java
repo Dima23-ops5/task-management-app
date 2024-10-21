adc285c8ab6c8d4d265d4714626c1324256951c9
Dmytro Makhanok <dmmhnk23@gmail.com>
2024-10-21 16:28:40 +0200

    added open api to project

--- src/main/java/com/example/task_management_app/controller/AuthenticationController.java
+++ src/main/java/com/example/task_management_app/controller/AuthenticationController.java
@@ -4,9 +4,10 @@ import com.example.task_management_app.dto.UserDto;
 import com.example.task_management_app.dto.UserLoginRequestDto;
 import com.example.task_management_app.dto.UserLoginResponseDto;
 import com.example.task_management_app.dto.UserRegistrationRequestDto;
-import com.example.task_management_app.repository.UserRepository;
 import com.example.task_management_app.security.AuthenticationService;
 import com.example.task_management_app.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,22 +17,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Authentication management", description = "Endpoints for managing authentication of users")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
     private final UserService userService;
-    private final UserRepository userRepository;
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Registration users", description = "Create and register a new user")
     public UserDto register(@RequestBody @Valid UserRegistrationRequestDto requestDto) {
         return userService.registerUser(requestDto);
     }
 
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Login user", description = "Log in the user if they already exist and return the user's token")
     public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto requestDto) {
         return authenticationService.authenticate(requestDto);
     }
--- src/main/java/com/example/task_management_app/dto/UserRegistrationResponseDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.task_management_app.dto;
-
-public record UserRegistrationResponseDto(
-        String userName,
-        String firstName,
-        String lastName,
-        String email
-) {
-}
