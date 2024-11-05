42b211598a62059b3a9ed2d47857829b71784a44
Dmytro Makhanok <dmmhnk23@gmail.com>
2024-11-05 19:47:15 +0100

    added tests for task and user services

--- src/test/java/com/example/task_management_app/service/internal/impl/UserServiceImplTest.java
+++ src/test/java/com/example/task_management_app/service/internal/impl/UserServiceImplTest.java
@@ -2,16 +2,21 @@ package com.example.task_management_app.service.internal.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 import com.example.task_management_app.dto.user.UserDto;
 import com.example.task_management_app.dto.user.UserRegistrationRequestDto;
+import com.example.task_management_app.dto.user.UserUpdateRequestDto;
+import com.example.task_management_app.dto.user.UserUpdateRoleDto;
 import com.example.task_management_app.mapper.UserMapper;
 import com.example.task_management_app.model.Role;
 import com.example.task_management_app.model.User;
 import com.example.task_management_app.repository.RoleRepository;
 import com.example.task_management_app.repository.UserRepository;
 import java.util.Optional;
+import java.util.Set;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -36,13 +41,7 @@ class UserServiceImplTest {
     @Test
     @DisplayName(value = "Register users correct")
     public void registerUser_Ok() {
-        User user = new User();
-        user.setId(1L);
-        user.setUserName("bob123");
-        user.setPassword("Bobpassword");
-        user.setEmail("bob@gmail.com");
-        user.setFirstName("Bob");
-        user.setLastName("Petrenko");
+        User user = createBobUser();
 
         UserRegistrationRequestDto requestDto = new UserRegistrationRequestDto(
                 user.getUsername(),
@@ -67,4 +66,106 @@ class UserServiceImplTest {
         assertEquals(excepted, actual);
     }
 
+    @Test
+    @DisplayName(value = "Should return user with updated role")
+    public void updateUserRoleByUserId_Ok() {
+        User user = createBobUser();
+        Role role = new Role();
+        role.setId(1L);
+        role.setRoleName(Role.RoleName.USER);
+        user.setRole(Set.of(role));
+
+        Role expectedRole = new Role();
+        expectedRole.setId(2L);
+        expectedRole.setRoleName(Role.RoleName.ADMIN);
+
+        User expected = new User();
+        expected.setId(user.getId());
+        expected.setUserName(user.getUserName());
+        expected.setFirstName(user.getFirstName());
+        expected.setLastName(user.getLastName());
+        expected.setEmail(user.getEmail());
+        expected.setPassword(user.getPassword());
+        expected.setRole(Set.of(expectedRole));
+
+        UserDto expectedUserDto = userToUserDto(expected);
+
+        UserUpdateRoleDto updateRoleDto = new UserUpdateRoleDto("ADMIN");
+
+        when(userRepository.findUserById(anyLong())).thenReturn(Optional.of(user));
+        when(roleRepository.findRoleByRoleName(any())).thenReturn(Optional.of(expectedRole));
+        when(userRepository.save(user)).thenReturn(expected);
+        when(userMapper.toDto(expected)).thenReturn(expectedUserDto);
+
+        UserDto actual = userServiceImpl.updateUserRoleById(user.getId(), updateRoleDto);
+
+        assertNotNull(actual);
+        assertEquals(expectedUserDto, actual);
+
+    }
+
+    @Test
+    @DisplayName(value = "Should find and return user by they id")
+    public void findUserById_Ok() {
+        User user = createBobUser();
+
+        UserDto expectedUserDto = userToUserDto(user);
+
+        when(userRepository.findUserById(anyLong())).thenReturn(Optional.of(user));
+        when(userMapper.toDto(user)).thenReturn(expectedUserDto);
+
+        UserDto actual = userServiceImpl.getUserById(user.getId());
+
+        assertNotNull(actual);
+        assertEquals(expectedUserDto, actual);
+    }
+
+    @Test
+    @DisplayName(value = "Should update and return updated user")
+    public void updateUserById_Ok() {
+        User user = createBobUser();
+
+        UserUpdateRequestDto requestDto =
+                new UserUpdateRequestDto("Alice",
+                        "Alice", "Bennett",
+                        "alice@gmail.com");
+        User expected = new User();
+        expected.setId(user.getId());
+        expected.setUserName(requestDto.userName());
+        expected.setFirstName(requestDto.firstName());
+        expected.setLastName(requestDto.lastName());
+        expected.setEmail(requestDto.email());
+        expected.setRole(user.getRole());
+        expected.setPassword(user.getPassword());
+
+        UserDto expectedUserDto = userToUserDto(expected);
+
+        when(userRepository.findUserById(anyLong())).thenReturn(Optional.of(user));
+        when(userMapper.updateUser(user, requestDto)).thenReturn(expected);
+        when(userRepository.save(expected)).thenReturn(expected);
+        when(userMapper.toDto(expected)).thenReturn(expectedUserDto);
+
+        UserDto actual = userServiceImpl.updateUser(user.getId(), requestDto);
+
+        assertNotNull(actual);
+        assertEquals(expectedUserDto, actual);
+    }
+
+    private User createBobUser() {
+        User user = new User();
+        user.setId(1L);
+        user.setUserName("bob123");
+        user.setPassword("Bobpassword");
+        user.setEmail("bob@gmail.com");
+        user.setFirstName("Bob");
+        user.setLastName("Petrenko");
+        return user;
+    }
+
+    private UserDto userToUserDto(User user) {
+        UserDto userDto = new UserDto(user.getId(),
+                user.getUserName(), user.getFirstName(),
+                user.getLastName(), user.getEmail());
+        return userDto;
+    }
 }
